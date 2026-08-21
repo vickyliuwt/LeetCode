926336318e04589d88d9298debfd7075658fd1d1
Weiting Liu <vickyliu.0425@gmail.com>
2026-08-21 18:10:50 -0400

    Time: 487 ms (90.46%), Space: 22.3 MB (54.04%) - LeetHub

--- 0015-3sum/0015-3sum.py
+++ 0015-3sum/0015-3sum.py
@@ -3,7 +3,7 @@ class Solution:
         nums.sort()
         n = len(nums)
         res = []
-        for i in range(n - 2):
+        for i in range(n -2):
             if nums[i] > 0:
                 break
             if i > 0 and nums[i] == nums[i - 1]:
@@ -24,3 +24,4 @@ class Solution:
                     while left < right and nums[right] == nums[right + 1]:
                         right -= 1
         return res
+        
