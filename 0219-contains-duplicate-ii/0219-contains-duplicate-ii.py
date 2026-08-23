a5fca1357478278607cd6e7e88a4414adab887fe
Weiting Liu <vickyliu.0425@gmail.com>
2026-08-23 01:03:46 -0400

    Time: 51 ms (24.77%), Space: 36.2 MB (69.92%) - LeetHub

---
+++ 0219-contains-duplicate-ii/0219-contains-duplicate-ii.py
@@ -0,0 +1,16 @@
+from typing import List
+
+class Solution:
+    def containsNearbyDuplicate(self, nums: List[int], k: int) -> bool:
+        window_set = set()
+        
+        for i in range(len(nums)):
+            if i > k:
+                window_set.remove(nums[i - k - 1])
+            
+            if nums[i] in window_set:
+                return True
+            
+            window_set.add(nums[i])
+            
+        return False
