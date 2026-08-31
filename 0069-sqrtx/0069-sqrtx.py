8441aaf337e04242c19664a809125b1f29012b9a
Weiting Liu <vickyliu.0425@gmail.com>
2026-08-30 21:03:14 -0400

    Time: 3 ms (69.45%), Space: 19.2 MB (56.8%) - LeetHub

---
+++ 0069-sqrtx/0069-sqrtx.py
@@ -0,0 +1,8 @@
+class Solution:
+    def mySqrt(self, x: int) -> int:
+        if x < 2:
+            return x
+
+        left = self.mySqrt(x >> 2) << 1
+        right = left + 1
+        return left if right * right > x else right
